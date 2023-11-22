9bddd30744c349594bb1cc0b6fd0330053ad5dc4
Koomaxx86 <bluejjw86@daum.net>
2023-11-22 09:54:25 +0900

    1122 0954

--- node.js/이벤트/event.js
+++ node.js/이벤트/event.js
@@ -2,29 +2,31 @@
 const EventEmitter = require('events');
 
 // EventEmitter(이벤트 발생자) 객체 생성
-// 이벤트객체.addListener( '이벤트명', () => {} )
 const myEvent = new EventEmitter();
-myEvent.addListener('event1', () => {
-  console.log('이벤트 1');
-});
 
 // 이벤트 리스너 등록
+// 이벤트객체.addListener( '이벤트명', () => {} )
+myEvent.addListener('event1', () => {
+    console.log('이벤트 1');
+});
+
 // 이벤트객체.on( '이벤트명', () => {} )
 myEvent.on('event2', () => {
   console.log('이벤트 2');
 });
+
 myEvent.on('event2', () => {
   console.log('이벤트 2 추가');
 });
 
 // 이벤트객체.once( '이벤트명', () => {} )
-// 이벤트 발생 시,  한 번만 실행
+// ✅ 이벤트 발생 시, 한 번만 실행
 myEvent.once('event3', () => {
   console.log('이벤트 3');
 }); // 한 번만 실행됨
 
 myEvent.emit('event1'); // 이벤트 호출
-myEvent.emit('event2'); // 이벤트 호출 (이벤트 2 추가)
+myEvent.emit('event2'); // 이벤트 호출
 
 myEvent.emit('event3');
 myEvent.emit('event3'); // 실행 안 됨
